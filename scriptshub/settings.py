7018c15f6c5e603ae068f540830e8ae45cff4b89
saqer <saqerzghool767@gmail.com>
2026-08-15 18:02:43 +0300

    Fix ALLOWED_HOSTS for Render deployment

--- scriptshub/settings.py
+++ scriptshub/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+ 
+
+ 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +29,11 @@ SECRET_KEY = 'django-insecure-i-_zu8yx^)qj+ein=jqev7pgn#rw8^a@4gx=04gpe&7+smen_+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# السماح بنطاق Render المحدد ونطاقك المحلي
+ALLOWED_HOSTS = ['scriptshub.onrender.com', 'localhost', '127.0.0.1']
+
+# أو السماح لجميع النطاقات (مريح ومناسب جداً للـ Deployment الحالي)
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
